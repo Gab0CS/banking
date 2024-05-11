8a87459b5de282f7cb9d714a873266dd537f4232
Gab0CS <gabocanseco@gmail.com>
2024-05-11 00:54:09 -0600

    Right sidebar and bank card finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Gabriel' };
+    const loggedIn = { 
+      firstName: 'Gabriel', 
+      lastName: 'Canseco', 
+      email: 'gabocanseco@gmail.com'
+    };
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +25,16 @@ const Home = () => {
                 totalCurrentBalance={122250.43}
             />
         </header>
+
+        Recent Transactions
       </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.45}, 
+          {currentBalance: 678.90}]}
+      />
     </section>
   )
 }
